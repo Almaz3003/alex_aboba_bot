de824f8db67c16c1bc3cb41c540056c06f65e31c
Almaz3003 <lunituni48@gmail.com>
2022-01-22 23:01:16 +0200

    first connection to DB

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5005709863:AAFsGYY_VADTsTn5PoYARIzEfw3aYdeiF8k"
 APP_URL = "https://alex-aboba-bot.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://lzbnaoxufnywib:eb59a61e0886b9e39378123ade7537b8a8a765b114470dfe85193a938bda536d@ec2-54-247-96-153.eu-west-1.compute.amazonaws.com:5432/dd5r3sgsomhgs3"
